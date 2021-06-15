9ee225a1971f8ca3f8d63fef842a9e47c1761061
muhittinpalamutcu <muhittinpalamutcu@gmail.com>
2021-06-15 16:18:03 +0300

    Police car and player interactions

--- Assets/Scripts/CarInputHandler.cs
+++ Assets/Scripts/CarInputHandler.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class CarInputHandler : MonoBehaviour
 {
-
     CarController carController;
+    public PoliceCarController policeCarController;
+
+    Rigidbody2D carRigidbody2D;
+
+    private bool gameOver = false;
 
     void Awake()
     {
         carController = GetComponent<CarController>();
-
+        carRigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     void Start()
@@ -21,10 +25,45 @@ public class CarInputHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 inputVector = Vector2.zero;
-        inputVector.x = Input.GetAxis("Horizontal");
-        inputVector.y = Input.GetAxis("Vertical");
+        if (!gameOver)
+        {
+            Vector2 inputVector = Vector2.zero;
+            inputVector.x = Input.GetAxis("Horizontal");
+            inputVector.y = Input.GetAxis("Vertical");
 
-        carController.SetInputVector(inputVector);
+            carController.SetInputVector(inputVector);
+            policeCarController.Player1CarVelocity = carRigidbody2D.velocity.magnitude;
+        }
+        else
+        {
+            carRigidbody2D.velocity = Vector2.zero;
+            carRigidbody2D.freezeRotation = true;
+            carController.SetInputVector(Vector2.zero);
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Border")
+        {
+            //Debug.Log("The player has collided with the wall!");
+            policeCarController.Chase = true;
+        }
+        if (collision.gameObject.tag == "PoliceCar")
+        {
+            Debug.Log("Player caught - game over");
+            gameOver = true;
+            policeCarController.GameOver = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Border")
+        {
+            //Debug.Log("The player no more collision with the wall!");
+            policeCarController.Chase = false;
+        }
     }
 }
--- Assets/Scripts/PoliceCarController.cs
+++ Assets/Scripts/PoliceCarController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PoliceCarController : MonoBehaviour
 {
-
     [HideInInspector]
     public Transform player1Car;
     public Rigidbody2D player1CarRigid;
@@ -12,24 +11,66 @@ public class PoliceCarController : MonoBehaviour
     public float speed;
     public float stopDistance;
     private Vector2 velocity = Vector2.zero;
+    private bool chase = false;
+    private float player1CarVelocity;
+    private bool gameOver;
+
+    Rigidbody2D policeCarRigidBody2d;
 
     void Start()
     {
         player1Car = GameObject.FindGameObjectWithTag("Player1Car").transform;
-
+        policeCarRigidBody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (player1Car != null)
+        if (player1Car != null && !gameOver)
         {
-            if (Vector2.Distance(transform.position, player1Car.position) > stopDistance)
+            if (Vector2.Distance(transform.position, player1Car.position) > stopDistance && player1CarVelocity > 5)
             {
                 transform.position = Vector2.SmoothDamp(transform.position, player1Car.position, ref velocity, 0.5f, speed);
                 transform.rotation = player1Car.rotation;
             }
-
+            else
+            {
+                //stop moving case 
+                if (player1CarVelocity < 5 || chase)
+                {
+                    transform.position = Vector2.SmoothDamp(transform.position, player1Car.position, ref velocity, 0.5f, speed);
+                    transform.rotation = player1Car.rotation;
+                }
+            }
         }
     }
+
+    public bool Chase
+    {
+        get { return chase; }
+        set { chase = value; }
+    }
+
+    public float Player1CarVelocity
+    {
+        get { return player1CarVelocity; }
+        set { player1CarVelocity = value; }
+    }
+
+    public bool GameOver
+    {
+        get { return gameOver; }
+        set { gameOver = value; }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player1Car")
+        {
+            Debug.Log("Player caught - game over");
+            policeCarRigidBody2d.velocity = Vector2.zero;
+            policeCarRigidBody2d.freezeRotation = true;
+        }
+    }
+
 }
